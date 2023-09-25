947039daba39ff9ceebec78942438bfbc6cb53f1
Pavel Dovlatov <pavdovlatov@yandex.com>
2023-09-25 11:19:03 +0300

    Altered error message for PaypalPaymentProcessor

--- src/PaymentProcessor/PaypalPaymentProcessor.php
+++ src/PaymentProcessor/PaypalPaymentProcessor.php
@@ -13,7 +13,7 @@ class PaypalPaymentProcessor
     public function pay(int $price): void
     {
         if ($price > 100000) {
-            throw new Exception('Too high price');
+            throw new Exception('[#14271] Transaction "c82711ca-7e67-41c8-9f35-5b965e645d12" failed: Too high price');
         }
 
         //process payment logic
